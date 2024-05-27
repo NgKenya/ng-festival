c1074c00ca986ee249aac2460675b9aef3851146
TechMadi <winnieamandela@gmail.com>
2024-05-27 15:36:06 +0300

    Fix:Unwanted sections

--- ng-web/src/app/services/util/util.service.ts
+++ ng-web/src/app/services/util/util.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
+import { ticketUrl } from 'src/app/const/data.const';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
-
 export class UtilService {
+  constructor() {}
 
-  constructor() { }
+  openNewPage(link: string) {
+    window.open(link, '_blank');
+  }
 
-
-
-  
+  getTickets() {
+    window.open(ticketUrl, '_blank');
+  }
 }
-
