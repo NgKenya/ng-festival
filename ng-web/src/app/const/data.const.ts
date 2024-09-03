946f30c56b8427833d6e0e8bc7146900f65de3fd
TechMadi <winnieamandela@gmail.com>
2024-09-03 15:08:00 +0300

    Data: Speakers Data

--- ng-web/src/app/const/data.const.ts
+++ ng-web/src/app/const/data.const.ts
@@ -1,5 +1,5 @@
 import { IPartner } from '../models/data.model';
-import { ITalk } from '../models/speaker.model';
+import { ISpeaker, ITalk } from '../models/speaker.model';
 
 export const NgKenyaPartners: IPartner[] = [
   {
@@ -20,7 +20,78 @@ export const NgKenyaPartners: IPartner[] = [
 ];
 
 export const ticketUrl = 'https://paydexp.com/ng-kenya-2024';
+export const SpeakerList: ISpeaker[] = [
+  {
+    name: 'Wycliffe Maina',
+    imageUrl: 'assets/Images/speakers/maina-wycliffe.jpg',
+    role: 'Angular Developer',
+    company: 'All Things TypeScript',
+  },
+  {
+    name: 'Christine Mukiri',
+    imageUrl: 'assets/Images/speakers/christine-mukiri.jpg',
+    role: 'Software Engineer',
+    company: 'Software Developer',
+  },
+  {
+    name: 'John Kamau',
+    imageUrl: 'assets/Images/speakers/john-kamau.jpg',
+    role: 'Software Developer',
+    company: 'Full stack Developer',
+  },
+  {
+    name: 'Karanja Chiuri',
+    imageUrl: 'assets/Images/speakers/karanja-chiuri.jpg',
+    role: 'Software Developer',
+    company: 'Full stack Developer',
+  },
 
+  {
+    name: 'Alex Muturi',
+    imageUrl: 'assets/Images/speakers/job-alex-muturi.jpg',
+    role: 'Frontend Engineer Lead',
+    company: 'Kyosk',
+  },
+
+  {
+    name: 'Ivy Walobwa',
+    imageUrl: 'assets/Images/speakers/ivy-walobwa.jpg',
+    role: 'Software Engineer',
+    company: 'Software Company',
+  },
+
+  {
+    name: 'Paul Chesa',
+    imageUrl: 'assets/Images/speakers/paul_chesa.png',
+    role: 'Software Engineer',
+    company: 'elewa',
+  },
+  {
+    name: 'Chris Achinga',
+    imageUrl: 'assets/Images/speakers/chris-achinga.jpg',
+    role: 'Software Engineer',
+    company: 'Hackmamba Inc.',
+  },
+  {
+    name: 'Joy Linda',
+    imageUrl: 'assets/Images/speakers/joy_linda.png',
+    role: 'Software Engineer',
+    company: 'Software Developer',
+  },
+  {
+    name: 'Wayne Gakuo',
+    imageUrl: 'assets/Images/speakers/wayne.jpg',
+    role: 'Frontend Engineer',
+    company: 'Sky.Garden',
+  },
+
+  // {
+  //   name: 'Winnie Mandela',
+  //   imageUrl: 'assets/Images/speakers/madi.jpeg',
+  //   role: 'Junior Quality Engineer',
+  //   company: 'Peer Carbon',
+  // },
+];
 export const TalkList: ITalk[] = [
   {
     startTime: new Date('2024-09-07T08:00:00'),
--- ng-web/src/app/modules/shared/components/speaker-card/speaker-card.component.ts
+++ ng-web/src/app/modules/shared/components/speaker-card/speaker-card.component.ts
@@ -1,12 +1,11 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ISpeaker } from 'src/app/models/speaker.model';
 
 @Component({
   selector: 'app-speaker-card',
   templateUrl: './speaker-card.component.html',
-  styleUrls: ['./speaker-card.component.scss']
+  styleUrls: ['./speaker-card.component.scss'],
 })
 export class SpeakerCardComponent {
-
-  @Input() speaker: any;
-
+  @Input() speaker!: ISpeaker;
 }
