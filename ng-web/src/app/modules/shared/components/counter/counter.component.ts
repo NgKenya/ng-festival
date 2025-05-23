4ea74cb4135c6961bf602b4c29378f2fa1b5d2b0
TechMadi <winnieamandela@gmail.com>
2025-05-23 16:19:30 +0300

    ui :get ticktes

--- ng-web/src/app/modules/shared/components/counter/counter.component.ts
+++ ng-web/src/app/modules/shared/components/counter/counter.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit, inject } from "@angular/core";
-import { Observable, interval, takeUntil } from "rxjs";
+import { Subscription } from "rxjs";
 import {
 	ngKenya2024Photos,
 	ngKenya2025CallForSpeakers,
 } from "src/app/const/data.const";
 import { UtilService } from "src/app/services/util/util.service";
+import {
+	CountdownService,
+	IDate,
+} from "src/app/services/countdown/countdown.service";
 
-interface IDate {
-	month: number;
-	day: number;
-	hour: number;
-	minute: number;
-	second: number;
-}
 @Component({
 	selector: "app-counter",
 	templateUrl: "./counter.component.html",
@@ -22,7 +19,6 @@ export class CounterComponent implements OnInit {
 	officialDate: Date = new Date("July 5 2025");
 	photoLink: string = ngKenya2024Photos;
 	callForSpeakersLink: string = ngKenya2025CallForSpeakers;
-	daysLeft: number = 0;
 	daysLeftDisplay: IDate = {
 		month: 0,
 		day: 0,
@@ -31,77 +27,18 @@ export class CounterComponent implements OnInit {
 		second: 0,
 	};
 	utilService = inject(UtilService);
-
-	// private subscription: Subscription;
+	countdownService = inject(CountdownService);
+	private countdownSubscription?: Subscription;
 
 	ngOnInit(): void {
-		this.getDateDifference();
-		this.startTimer();
-	}
-
-	getDateDifference() {
-		const now = new Date();
-		const diffinDays = this.officialDate.getTime() - now.getTime();
-		this.daysLeft = Math.ceil(diffinDays);
-	}
-	startTimer() {
-		interval(1000)
-			.pipe(
-				takeUntil(
-					new Observable((subscriber) => {
-						const checkInterval = setInterval(() => {
-							if (this.isEventOver()) {
-								subscriber.complete();
-								clearInterval(checkInterval);
-							}
-						}, 1000);
-					}),
-				),
-			)
-			.subscribe(() => {
-				this.getDateDifference();
-				// const totalSeconds = this.daysLeft;
-				this.daysLeftDisplay = this.calculateRemainingTime(this.daysLeft);
+		this.countdownSubscription = this.countdownService
+			.startCountdown(this.officialDate)
+			.subscribe((remaining: IDate) => {
+				this.daysLeftDisplay = remaining;
 			});
 	}
 
-	calculateRemainingTime(totalMilliseconds: number): {
-		month: number;
-		day: number;
-		hour: number;
-		minute: number;
-		second: number;
-	} {
-		const millisecondsPerMonth = 1000 * 60 * 60 * 24 * 30;
-		const millisecondsPerDay = 1000 * 60 * 60 * 24;
-		const millisecondsPerHour = 1000 * 60 * 60;
-		const millisecondsPerMinute = 1000 * 60;
-
-		// picked month
-		const months = Math.floor(totalMilliseconds / millisecondsPerMonth);
-		let remainingMilliseconds = totalMilliseconds % millisecondsPerMonth;
-
-		const days = Math.floor(remainingMilliseconds / millisecondsPerDay);
-		remainingMilliseconds %= millisecondsPerDay;
-
-		const hours = Math.floor(remainingMilliseconds / millisecondsPerHour);
-		remainingMilliseconds %= millisecondsPerHour;
-
-		const minutes = Math.floor(remainingMilliseconds / millisecondsPerMinute);
-		remainingMilliseconds %= millisecondsPerMinute;
-
-		const seconds = Math.floor(remainingMilliseconds / 1000);
-
-		return {
-			month: months,
-			day: days,
-			hour: hours,
-			minute: minutes,
-			second: seconds,
-		};
-	}
-
-	isEventOver() {
-		return new Date() >= this.officialDate;
+	ngOnDestroy(): void {
+		this.countdownSubscription?.unsubscribe();
 	}
 }
