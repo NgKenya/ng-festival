06483969033feba8ed2498490c5fb362f91fbc65
TechMadi <winnieamandela@gmail.com>
2025-06-30 23:53:22 +0300

    upgrde : updating file structure

--- ng-web/src/app/app.component.ts
+++ ng-web/src/app/app.component.ts
@@ -1,30 +1,32 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { NavigationEnd, Router, Event } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { Component, inject, OnInit } from "@angular/core";
+import { NavigationEnd, Router, Event, RouterModule } from "@angular/router";
 
-import { IStaticMethods } from 'preline/preline';
+import { IStaticMethods } from "preline/preline";
 declare global {
-  interface Window {
-    HSStaticMethods: IStaticMethods;
-  }
+	interface Window {
+		HSStaticMethods: IStaticMethods;
+	}
 }
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
+	imports: [CommonModule, RouterModule],
+	standalone: true,
 })
 export class AppComponent implements OnInit {
-  title = 'ng-web';
-  router = inject(Router);
+	title = "ng-web";
+	router = inject(Router);
 
-  ngOnInit() {
-    this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationEnd) {
-        setTimeout(() => {
-          window.HSStaticMethods.autoInit();
-        }, 100);
-      }
-    });
-  }
+	ngOnInit() {
+		this.router.events.subscribe((event: Event) => {
+			if (event instanceof NavigationEnd) {
+				setTimeout(() => {
+					window.HSStaticMethods.autoInit();
+				}, 100);
+			}
+		});
+	}
 }
