b191c26f9b9d407cb8edce7da2ea875d4f8f434b
TechMadi <winnieamandela@gmail.com>
2025-07-03 13:16:18 +0300

    feature :   file swap

--- ng-web/src/app/modules/home/home.module.ts
+++ ng-web/src/app/modules/home/home.module.ts
@@ -3,7 +3,7 @@ import { CommonModule, NgOptimizedImage } from "@angular/common";
 
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./components/home/home.component";
-import { SpeakersComponent } from "./components/speakers/speakers.component";
+import { SpeakersComponent } from "../../pages/speakers/speakers.component";
 import { ScheduleComponent } from "./components/schedule/schedule.component";
 import { ContactUsComponent } from "./components/contact-us/contact-us.component";
 import { SharedModule } from "../shared/shared.module";
